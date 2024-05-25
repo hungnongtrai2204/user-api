5ffbf505b80c79744f179036dbbee760d67cdac6
Vu Quoc Hung <92137335+hungnongtrai2204@users.noreply.github.com>
2024-05-25 11:42:17 +0700

    Update PORT

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-const userRouter = require('./Routes/User.route');
-const addressRouter = require('./Routes/Address.route');
+const userRouter = require("./Routes/User.route");
+const addressRouter = require("./Routes/Address.route");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_CONNECTION).then(()=>{
-    console.log('Connected MongoDB');
-})
+mongoose.connect(process.env.MONGODB_CONNECTION).then(() => {
+  console.log("Connected MongoDB");
+});
 
-app.use('/users',userRouter);
-app.use('/address',addressRouter);
+app.use("/users", userRouter);
+app.use("/address", addressRouter);
 
-app.listen(8000,()=>{
-    console.log('Server is running on port 8000');
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
